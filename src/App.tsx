e5f45469c425df16729c9cfafc585b4f1ba9ef83
aryan02420 <aryan02420@gmail.com>
2023-09-25 14:14:45 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <main>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      {/* <OkrDetailsPage id="1" /> */}
+      {/* <OkrDetailsPage id="2" /> */}
+      {/* <TaskDetailsPage id="1" /> */}
+      {/* <TaskDetailsPage id="2" /> */}
+    </main>
+  );
+}
+
+export default App;
